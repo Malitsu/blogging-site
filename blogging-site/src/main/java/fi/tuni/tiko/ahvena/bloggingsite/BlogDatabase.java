5f07977ceb10a8c4d1a699f9499bba0e06f311be
Tiina <malitsu@gmail.com>
2020-04-29 18:22:03 +0300

    Create separate module for interaction with the server.

--- blogging-site/src/main/java/fi/tuni/tiko/ahvena/bloggingsite/BlogDatabase.java
+++ blogging-site/src/main/java/fi/tuni/tiko/ahvena/bloggingsite/BlogDatabase.java
@@ -3,6 +3,8 @@ package fi.tuni.tiko.ahvena.bloggingsite;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BlogDatabase extends CrudRepository<BlogPost, Integer> {
 }
---
+++ frontend/src/services/posts.js
@@ -0,0 +1,45 @@
+const fetch = require('node-fetch')
+
+const baseUrl = 'http://localhost:8080/'
+
+const getPosts = () => {
+  const request = fetch(baseUrl, {
+    method: 'GET'
+  })
+  return request.then(jsonObject => jsonObject.json())
+    .then(response => response.data)
+}
+
+const deletePost = (id) => {
+  const request = fetch(baseUrl + id, {
+    method: 'DELETE'
+  })
+  return request.then(jsonObject => jsonObject.json())
+    .then(response => response.data)
+}
+
+const addPost = (newPost) => {
+  const request = fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPost)
+  })
+  return request.then(jsonObject => jsonObject.json())
+    .then(response => response.data)
+}
+
+const updatePost = (id, newPost) => {
+  const request = fetch(baseUrl + id, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPost)
+  })
+  return request.then(jsonObject => jsonObject.json())
+    .then(response => response.data)
+}
+
+export default { getPosts, deletePost, addPost, updatePost }
