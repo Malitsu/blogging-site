ca588494df1f87ff8abff8c64910e704cc7e392a
Tiina <malitsu@gmail.com>
2020-05-26 16:18:10 +0300

    Fix validation problems.

--- src/main/app/src/PostTemplate/PostTemplate.js
+++ src/main/app/src/PostTemplate/PostTemplate.js
@@ -1,25 +1,31 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react'
 import './PostTemplate.css'
 
-function TemplateItem(props) {
+function TemplateItem (props) {
   return (
-    <div className="templateItem"> 
+    <div className="templateItem">
       <h4>{props.title}</h4>
-        <p>{props.writer}</p>
-        <p>{props.time}</p>
-        <p>{props.body}</p>
+      <p>{props.writer}</p>
+      <p>{props.time}</p>
+      <p>{props.body}</p>
     </div>
   )
-
 }
-function PostTemplate(props) {
-  const posts = props.posts;
+function PostTemplate (props) {
+  const posts = props.posts
   const template = posts.map(post =>
-    <TemplateItem title={post.title} writer={post.writer} time={post.time} body={post.body}/>)
-    return (
-      <div className="template">
-        {template}
-      </div>
-    )
+    <TemplateItem
+      key={post.id}
+      title={post.title}
+      writer={post.writer}
+      time={post.time}
+      body={post.body}
+    />)
+  return (
+    <div className="template">
+      {template}
+    </div>
+  )
 }
-export default PostTemplate;
+export default PostTemplate
