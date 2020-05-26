0e5dd8982e5cce5b8b0db9b0594f6c1a01afa8eb
Tiina <malitsu@gmail.com>
2020-05-26 17:19:28 +0300

    Add comment possibility to backend.

---
+++ src/main/java/fi/tuni/tiko/ahvena/bloggingsite/Comment.java
@@ -0,0 +1,82 @@
+package fi.tuni.tiko.ahvena.bloggingsite;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+/*
+This class holds the BlogComment object which has parameters
+String title, String body, Date time and String writer.
+ */
+@Entity
+public class Comment {
+    public String title;
+    public Date time;
+    public String writer;
+    public int likes;
+
+    @Override
+    public String toString() {
+        return "BlogComment{" +
+                "title='" + title + '\'' +
+                ", time=" + time +
+                ", writer='" + writer + '\'' +
+                ", likes=" + likes +
+                ", body='" + body + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Column(columnDefinition = "longtext")
+    public String body;
+
+    @Id
+    @GeneratedValue
+    public int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public int getLikes() { return likes; }
+
+    public void setLikes(int likes) { this.likes = likes; }
+
+    public Comment(String title, String body, Date time, String writer, int likes) {
+        this.title = title;
+        this.body = body;
+        this.time = time;
+        this.writer = writer;
+        this.likes = likes;
+    }
+    public Comment() {}
+}
